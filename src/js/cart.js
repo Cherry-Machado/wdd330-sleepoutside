b36a7452d2d36733ab785b15f14f74362d00cec9
Cherry-Machado <Cherry5467@gmail.com>
2025-05-27 01:21:57 -0300

    Finish week 3 adding items to the list

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,28 +1,66 @@
-import { getLocalStorage } from "./utils.mjs";
+import {
+  getLocalStorage,
+  setLocalStorage,
+  priceTotal,
+  loadHeaderFooter,
+} from "./utils.mjs";
+
+loadHeaderFooter();
+
+function deleteCartContent(event) {
+  const itemId = event.target.getAttribute("data-id");
+  let cartItems = getLocalStorage("so-cart");
+
+  const itemIndex = cartItems.findIndex((item) => item.Id === itemId);
+  cartItems[itemIndex].quantity -= 1;
+
+  if (cartItems[itemIndex].quantity <= 0) {
+    cartItems = cartItems.filter((item) => item.Id !== itemId);
+  }
+
+  setLocalStorage("so-cart", cartItems);
+  renderCartContents();
+}
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  const total = priceTotal(
+    cartItems,
+    (item) => item.FinalPrice * item.quantity,
+  );
+  document.querySelector(".cart-total__amount").textContent = total;
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  document.querySelectorAll(".cart-card__delete").forEach((button) => {
+    button.addEventListener("click", deleteCartContent);
+  });
 }
 
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+  const newItem = `
+  <li class="cart-card divider">
+    <a href="#" class="cart-card__image">
+      <img
+        src="${item.Images.PrimaryMedium}"
+        alt="${item.Name}"
+      />
+    </a>
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: ${item.quantity}</p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>  
+    <span class="cart-card__delete" data-id=${item.Id}>X</span>
+  </li>`;
 
   return newItem;
 }
+
+document
+  .querySelector("button.checkout")
+  .addEventListener("click", () =>
+    window.location.replace("/checkout/index.html"),
+  );
 
 renderCartContents();
