0f7ac30af3ea67b4a2e0de66c909bb313114a6ff
Cherry-Machado <Cherry5467@gmail.com>
2025-06-07 04:51:11 -0300

    Updating

--- dist/assets/ExternalServices-C6hio2pZ.js
+++ dist/assets/ExternalServices-C6hio2pZ.js
@@ -1,1 +1,31 @@
-const o="https://wdd330-backend.onrender.com";async function r(s){const t=await s.json();if(s.ok)return t;throw{name:"servicesError",message:t}}class c{constructor(){}async getData(t){const e=await fetch(`${o}/products/search/${t}`);return(await r(e)).Result}async findProductById(t){const e=await fetch(`${o}/product/${t}`);return(await r(e)).Result}async checkout(t){const e={method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(t)};try{const a=await fetch(`${o}/checkout/`,e);return await r(a)}catch(a){throw new Error("Error en el servidor: "+a.message)}}}export{c as E};
+const o = "https://wdd330-backend.onrender.com";
+async function r(s) {
+  const t = await s.json();
+  if (s.ok) return t;
+  throw { name: "servicesError", message: t };
+}
+class c {
+  constructor() {}
+  async getData(t) {
+    const e = await fetch(`${o}/products/search/${t}`);
+    return (await r(e)).Result;
+  }
+  async findProductById(t) {
+    const e = await fetch(`${o}/product/${t}`);
+    return (await r(e)).Result;
+  }
+  async checkout(t) {
+    const e = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(t),
+    };
+    try {
+      const a = await fetch(`${o}/checkout/`, e);
+      return await r(a);
+    } catch (a) {
+      throw new Error("Error en el servidor: " + a.message);
+    }
+  }
+}
+export { c as E };
