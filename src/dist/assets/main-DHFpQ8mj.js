d79d329b1144c37f97b5b8e2c01a1cbe575f8e54
Cherry-Machado <Cherry5467@gmail.com>
2025-06-06 23:40:54 -0300

    Updating

--- src/dist/assets/main-DHFpQ8mj.js
+++ src/dist/assets/main-DHFpQ8mj.js
@@ -1,1 +1,11 @@
-import{l as n}from"./utils-D05f2JY9.js";n();const a=document.getElementById("newsletter-form");a.addEventListener("submit",function(l){l.preventDefault();const e=a.querySelector("input[type='email']"),t=e.value;t?(alert(`Thank you for signing up, ${t}!`),e.value=""):alert("Please enter a valid email address.")});
+import { l as n } from "./utils-D05f2JY9.js";
+n();
+const a = document.getElementById("newsletter-form");
+a.addEventListener("submit", function (l) {
+  l.preventDefault();
+  const e = a.querySelector("input[type='email']"),
+    t = e.value;
+  t
+    ? (alert(`Thank you for signing up, ${t}!`), (e.value = ""))
+    : alert("Please enter a valid email address.");
+});
