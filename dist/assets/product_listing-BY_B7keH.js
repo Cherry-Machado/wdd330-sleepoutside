eae5532a089d7d95220a09a22091ebba64f78c7e
Cherry-Machado <Cherry5467@gmail.com>
2025-06-07 01:58:25 -0300

    Updating

---
+++ dist/assets/product_listing-BY_B7keH.js
@@ -0,0 +1,8 @@
+import{c as s,l as o,b as i}from"./utils-CpYJUKXc.js";import{E as n}from"./ExternalServices-BOkwevmt.js";function d(t){return`<li class="product-card">
+                <a href="/product_pages/?product=${t.Id}">
+                <img src="${t.Images.PrimaryLarge}" alt="Image of ${t.Name}">
+                <h2 class="card__brand">${t.Brand.Name}</h2>
+                <h3 class="card__name">${t.Name}</h3>
+                <p class="product-card__price">$${t.FinalPrice}</p>
+                </a>
+            </li>`}class l{constructor(e,a,c){this.category=e,this.dataSource=a,this.listElement=c}async init(){const e=await this.dataSource.getData(this.category);this.renderList(e),document.querySelector("#product-category").textContent=this.category}renderList(e){s(d,this.listElement,e)}}o();const r=i("category"),m=new n,u=document.querySelector(".product-list"),g=new l(r,m,u);document.querySelector("#current-category").textContent=r;g.init();
