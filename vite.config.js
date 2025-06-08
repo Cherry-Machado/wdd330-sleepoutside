64ffccf5dc58d7e5ecbf9825c13f8ea2c499405b
Cherry-Machado <Cherry5467@gmail.com>
2025-06-07 21:04:35 -0300

    Updating

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        product_listing: resolve(__dirname, "src/product_listing/index.html"),
+        product: resolve(__dirname, "src/product_pages/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
-        success: resolve(__dirname, "src/checkout/success.html"),
-        product: resolve(__dirname, "src/product_pages/index.html"),
-        product_listing: resolve(__dirname, "src/product_listing/index.html"),
+        success: resolve(__dirname, "src/checkout/success.html")
       },
     },
   },
