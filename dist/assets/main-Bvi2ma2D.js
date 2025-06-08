525d51e54c558fd0478df12a4aa28241a36ffe85
Cherry-Machado <Cherry5467@gmail.com>
2025-06-07 21:51:45 -0300

    Updating

---
+++ dist/assets/main-Bvi2ma2D.js
@@ -0,0 +1,1 @@
+import{l as n}from"./utils-zCIKEsvN.js";n();const a=document.getElementById("newsletter-form");a.addEventListener("submit",function(l){l.preventDefault();const e=a.querySelector("input[type='email']"),t=e.value;t?(alert(`Thank you for signing up, ${t}!`),e.value=""):alert("Please enter a valid email address.")});
